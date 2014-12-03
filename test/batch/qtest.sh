17cc39cb89d00444df1eafc282a4b017024c867c
rgoldouz <goldouzian@Qsystem.particles.ipm.ir>
2014-12-03 12:25:24 +0330

    Adding a cfg files

---
+++ test/batch/qtest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export DPM_HOST=se1.particles.ipm.ac.ir
+export DPNS_HOST=se1.particles.ipm.ac.ir
+#lcg-ls "srm://se1.particles.ipm.ac.ir:8446/srm/managerv2?SFN=/dpm/particles.ipm.ac.ir/home/cms/store/user/goldouzian"
+cd /home/goldouzian/CMSSW_5_3_7/src/myanalysis/Atq/test/
+eval `scramv1 runtime -sh`
+export LD_LIBRARY_PATH=/home/goldouzian:${LD_LIBRARY_PATH}
+#setenv DPNS_HOST se1.particles.ipm.ac.ir
+#setenv DPM_HOST se1.particles.ipm.ac.ir
+#setenv LD_LIBRARY_PATH  /home/goldouzian:${LD_LIBRARY_PATH}
+cmsRun top-s-ch_cfg.py
---
+++ test/batch/realdata1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export DPM_HOST=se1.particles.ipm.ac.ir
+export DPNS_HOST=se1.particles.ipm.ac.ir
+#lcg-ls "srm://se1.particles.ipm.ac.ir:8446/srm/managerv2?SFN=/dpm/particles.ipm.ac.ir/home/cms/store/user/goldouzian"
+cd /home/goldouzian/CMSSW_5_3_7/src/myanalysis/Atq/test/
+eval `scramv1 runtime -sh`
+export LD_LIBRARY_PATH=/home/goldouzian:${LD_LIBRARY_PATH}
+cmsRun realdata1_cfg.py
+
